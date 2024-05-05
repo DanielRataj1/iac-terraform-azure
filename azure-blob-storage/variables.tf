52d1ada79b8c2ce444a27e99945c37fd16864707
DanielRataj1 <127019047+DanielRataj1@users.noreply.github.com>
2024-05-05 17:34:30 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,20 @@
 
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
+variable "blob_name" {
+  type = string
+}
+
+variable "blob_content" {
+  type = string
+}
